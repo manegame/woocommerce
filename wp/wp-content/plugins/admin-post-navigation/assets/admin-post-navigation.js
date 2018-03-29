e94cc9311fdc87c87ee92f14f772722cbd1e7a3c
manegame <manusnijhoff@gmail.com>
2018-03-29 20:31:56 +0200

    remove the now ignored wp dir

--- wp/wp-content/plugins/admin-post-navigation/assets/admin-post-navigation.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {
-	$('#admin-post-nav').appendTo($('#wpbody-content .wrap:first ' + c2c_apn.tag + ':first'));
-	$('#adminpostnav, label[for="adminpostnav-hide"]').hide();
-});
